47de7c24304d6cfbcadf42be8d4394606a96cb96
Pavel Rozhkov <prozskov@gmail.com>
2026-02-12 14:12:02 +0100

    Add method to modify bodies for references and implement reference retrieval logic
    - Introduced `modify_bodies_for_references` method to handle references metadata and retrieve associated resources.
    - Added logic to search for resources based on specified profiles and resource types, with improved logging for missing resources.
    - Defined constant `ATTEMPTS_TO_GET_ENTRIES` to limit the number of entries processed during resource retrieval.

--- lib/inferno_suite_generator/test_modules/basic_test.rb
+++ lib/inferno_suite_generator/test_modules/basic_test.rb
@@ -18,6 +18,7 @@ module InfernoSuiteGenerator
     def_delegators "self.class", :demodata, :metadata
 
     NOT_FOUND_STATUS = 404
+    ATTEMPTS_TO_GET_ENTRIES = 10.freeze
 
     def resource_payload_for_input
       payload = resource_body_by_resource_type(resource_type).first
@@ -107,6 +108,83 @@ module InfernoSuiteGenerator
       end
 
       resource_filtered_by_profile
+    end
+
+    def modify_bodies_for_references(body)
+      info "Body to modify for references: #{body.inspect}"
+      # NOTE: Example of references_metadata:
+      # references:
+      # - :path: MedicationStatement.basedOn
+      #   :profiles:
+      #   - http://hl7.org/fhir/StructureDefinition/MedicationRequest
+      #   - http://hl7.org/fhir/StructureDefinition/CarePlan
+      #   - http://hl7.org/fhir/StructureDefinition/ServiceRequest
+      #   :resource_types:
+      #   - MedicationRequest
+      #   - CarePlan
+      #   - ServiceRequest
+      # - :path: MedicationStatement.partOf
+      #   :profiles:
+      #   - http://hl7.org/fhir/StructureDefinition/MedicationAdministration
+      #   - http://hl7.org/fhir/StructureDefinition/MedicationDispense
+      #   - http://hl7.org/fhir/StructureDefinition/MedicationStatement
+      #   - http://hl7.org/fhir/StructureDefinition/Procedure
+      #   - http://hl7.org/fhir/StructureDefinition/Observation
+      #   :resource_types:
+      #   - MedicationAdministration
+      #   - MedicationDispense
+      #   - MedicationStatement
+      #   - Procedure
+      #   - Observation
+      references_metadata_arr = metadata.references
+      references_to_set_for_paths = references_metadata_arr.map do |reference_metadata|
+        is_found = false
+        reference_to_set_for_path = {
+          reference: nil,
+          path: reference_metadata[:path]
+        } 
+        reference_metadata[:resource_types].each do |resource_type|
+          fhir_search(resource_type)
+          if response.status != 200
+            info "Can't search for #{resource_type} resources. Skipping this resource type..."
+            next
+          end 
+          bundle = resource
+          if bundle.nil?
+            info "Can't get bundle for #{resource_type} resources. Skipping this resource type..."
+            next
+          end
+          if bundle.entry.nil?
+            info "Bundle entry is nil. Skipping this resource type..."
+            next
+          end
+          if bundle.entry.length == 0
+            info "No #{resource_type} resources found. Skipping this resource type..."
+            next
+          end
+          bundle.entry[0...ATTEMPTS_TO_GET_ENTRIES].each do |entry|
+            resource = entry.resource
+            if resource.resourceType == resource_type && any_profile_matches_with_target_profiles(resource, reference_metadata[:profiles])
+              reference_to_set_for_path[:reference] = "#{resource.resourceType}/#{resource.id}"
+              is_found = true
+              break
+            end
+          end
+          break if is_found
+        end
+        reference_to_set_for_path
+      end
+      info "References to set for paths: #{references_to_set_for_paths.inspect}"
+      body
+    end
+
+    def any_profile_matches_with_target_profiles(resource, target_profiles)
+      return false if resource.meta.nil?
+      return false if resource.meta.profile.nil?
+
+      resource.meta.profile.any? do |profile|
+        target_profiles.include?(profile)
+      end
     end
 
     def resource_body_list
