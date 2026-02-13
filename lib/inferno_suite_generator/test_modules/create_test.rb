0f2c940a6abd9a5fe14717d2685de4d28a79f4be
Pavel Rozhkov <prozskov@gmail.com>
2026-02-13 18:12:14 +0100

    Fix CreateTest to handle nil resource IDs gracefully, preventing potential errors during reference processing.

--- lib/inferno_suite_generator/test_modules/create_test.rb
+++ lib/inferno_suite_generator/test_modules/create_test.rb
@@ -41,6 +41,8 @@ module InfernoSuiteGenerator
         next if current_reference[:resource_types].empty?
 
         resource_id = references_keeper.references_for_resource_type(current_reference[:resource_types].first).first
+        next if resource_id.nil?
+
         [path, { "reference" => "#{current_reference[:resource_types].first}/#{resource_id}" }]
       end
       resource_data = SetByPath.multi_set_by_path(resource_data, paths_and_values_to_set)
