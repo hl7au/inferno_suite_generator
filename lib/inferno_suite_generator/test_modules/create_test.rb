18cbfe4f39cceb2de873405cbd4351baf8bcae0c
Pavel Rozhkov <prozskov@gmail.com>
2026-02-13 18:17:47 +0100

    Refactor CreateTest to compact paths and values during resource data processing, enhancing robustness in reference handling.

--- lib/inferno_suite_generator/test_modules/create_test.rb
+++ lib/inferno_suite_generator/test_modules/create_test.rb
@@ -45,7 +45,7 @@ module InfernoSuiteGenerator
 
         [path, { "reference" => "#{current_reference[:resource_types].first}/#{resource_id}" }]
       end
-      resource_data = SetByPath.multi_set_by_path(resource_data, paths_and_values_to_set)
+      resource_data = SetByPath.multi_set_by_path(resource_data, paths_and_values_to_set.compact)
       info "References keeper: #{references_keeper.inspect}"
       info "Resource data with references (JSON): #{resource_data.to_json}"
       FHIR.from_contents(resource_data.to_json)
