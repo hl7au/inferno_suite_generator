5a128f1c97aecb7c1562c030f34e9b4f66f2843b
Pavel Rozhkov <prozskov@gmail.com>
2026-02-19 16:52:42 +0100

    Remove unnecessary request re-adding in create_test.rb

--- lib/inferno_suite_generator/test_modules/create_test.rb
+++ lib/inferno_suite_generator/test_modules/create_test.rb
@@ -74,7 +74,7 @@ module InfernoSuiteGenerator
       info "Requests before: #{requests.count}"
       fhir_search(resource_type, tags: ["must-be-hidden"])
       info "Requests after: #{requests.count}"
-      requests << requests.delete_at(-1)
+      requests.delete_at(-1)
       info "Requests after delete: #{requests.count}"
       unless response[:status] == SUCCESS_RESPONSE_STATUS
         info "Can't search for #{resource_type} resources. Skipping this resource type..."
