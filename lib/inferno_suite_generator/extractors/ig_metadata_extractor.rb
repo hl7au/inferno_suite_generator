8452bd8c06df8763c386b0ee5d673030da667a16
Pavel Rozhkov <prozskov@gmail.com>
2026-08-07 12:18:00 +0400

    Refactor IGMetadataExtractor to streamline metadata extraction from resources

--- lib/inferno_suite_generator/extractors/ig_metadata_extractor.rb
+++ lib/inferno_suite_generator/extractors/ig_metadata_extractor.rb
@@ -33,33 +33,28 @@ module InfernoSuiteGenerator
         ig_resources.cs_resources
       end
 
+      def extract_group_metadata(resource, profile, metadata, ig_resources)
+        begin
+          GroupMetadataExtractor.new(resource, profile, metadata, ig_resources).group_metadata
+        rescue StandardError => e
+          warn "Error extracting metadata for profile #{profile} of resource #{resource.type}: #{e.message}"
+          nil
+        end
+      end
+
+      def extract_resource_profiles(cs_resource)
+        all_profiles = [*cs_resource.supportedProfile, cs_resource.profile].compact.uniq.map { |p| p.split("|").first }.uniq
+        all_profiles.reject { |profile| config_keeper.skip_metadata_extraction?(profile, cs_resource.type) }.compact
+      end
+
+      def extract_metadata_for_cs_resource(cs_resource)
+        extract_resource_profiles(cs_resource).map do |profile|
+          extract_group_metadata(cs_resource, profile, metadata, ig_resources)
+        end.compact
+      end
+
       def add_metadata_from_resources
-        supported_profile_groups = resources_in_capability_statement.flat_map do |resource|
-          resource.supportedProfile&.map do |supported_profile|
-            supported_profile = supported_profile.split("|").first
-            next if config_keeper.skip_metadata_extraction?(supported_profile, resource.type)
-            begin
-              GroupMetadataExtractor.new(resource, supported_profile, metadata, ig_resources).group_metadata
-            rescue StandardError => e
-              warn "Error extracting metadata for supported profile #{supported_profile} of resource #{resource.type}: #{e.message}"
-              nil
-            end
-          end
-        end.compact
-
-        profile_groups = resources_in_capability_statement.flat_map do |resource|
-          next unless resource.profile.present?
-          next if config_keeper.skip_metadata_extraction?(resource.profile, resource.type)
-          begin
-            GroupMetadataExtractor.new(resource, resource.profile, metadata, ig_resources).group_metadata
-          rescue StandardError => e
-            warn "Error extracting metadata for profile #{resource.profile} of resource #{resource.type}: #{e.message}"
-            nil
-          end
-        end.compact
-
-        metadata.groups = supported_profile_groups + profile_groups
-
+        metadata.groups = resources_in_capability_statement.flat_map(&method(:extract_metadata_for_cs_resource)).compact
         metadata.postprocess_groups(ig_resources)
       end
     end
